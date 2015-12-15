83706d1caeb675410fd60d8da75210c0f2de59fc
designeng <denissavenok@gmail.com>
2015-12-15 17:31:46 +0300

    href fixed

--- src/components/navigation/section/index.js
+++ src/components/navigation/section/index.js
@@ -3,13 +3,17 @@ import React from 'react';
 export default class NavigationSection extends React.Component {
 
     componentWillMount() {
-        this.setState({items: [{name: "Acura"}, {name: "Nissan"}, {name: "Ford"}]});
+        this.setState({items: [
+            {name: "Acura", href: "/acura/"}, 
+            {name: "Nissan", href: "/nissan/"}, 
+            {name: "Ford" , href: "/ford/"}
+        ]});
     }
 
     render() {
         var items = Object.keys(this.state.items).map(idx => {
             return <li key={idx} className="nav-mobile-item">
-                <a className="nav-mobile-item__link" href="/acura/">
+                <a className="nav-mobile-item__link" href={this.state.items[idx].href} >
                     {this.state.items[idx].name}
                 </a></li>
         });
