2a2a8c6e93b99a573c7b43c5f99352ee9b3db2de
designeng <denissavenok@gmail.com>
2015-12-16 15:06:50 +0300

    items & brands pathes tests passed as expected

--- test/api/falcor/models/index.js
+++ test/api/falcor/models/index.js
@@ -2,19 +2,33 @@ var app      = require('../../../../server/app.js');
 
 var request  = require('supertest');
 var apitest = request(app);
+var assert = require('chai').assert;
+
+var getResponseBody = function (response) {
+    return JSON.parse(response.text)['jsonGraph']
+}
 
 describe('integration', function () {
     beforeEach(function () {
 
     });
 
-    it('/navigation/model.json path exists and should accept get request', function (done) {
-        apitest.get('/navigation/model.json?paths=[["names"]]&method=get')
-            .expect('Content-Type', /json/)
+    it('/navigation/model.json path exists, should accept get request and "items" path should return array with 3 elements', function (done) {
+        apitest.get('/navigation/model.json?paths=[["items"]]&method=get')
             .expect(200)
-            .end((err, res) => {
-                console.log("res", res);
-                if (err) return done(err);
+            .end((error, response) => {
+                if (error) return done(error);
+                assert.equal(getResponseBody(response)['items']['value'].length, 3);
+                done();
+            });
+    });
+
+    it('/navigation/model.json "brands" path should return array with 3 elements', function (done) {
+        apitest.get('/navigation/model.json?paths=[["brands"]]&method=get')
+            .expect(200)
+            .end((error, response) => {
+                if (error) return done(error);
+                assert.equal(getResponseBody(response)['brands']['value'].length, 5);
                 done();
             });
     });
