bdf02b5da6745c38482d033de06150a3ccb1c1dc
designeng <denissavenok@gmail.com>
2016-01-11 16:38:48 +0300

    added components/news/News.css; styles for news list

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import connectModel from 'connect-data-decorator';
 
+import styles from './News.css';
+
 @connectModel({
     sourcePath: '/news/model.json'
 })
@@ -23,24 +25,17 @@ export default class NewsList extends React.Component {
     }
 
     render() {
-        const newsListClass     = "top-news-list top-news-list_flipping";
-        const aClass            = "top-news-item wrap";
-        const imgWrapperClass   = "image-wrapper image-wrapper_big cell";
-        const imgClass          = "top-news-item__image";
-        const captionClass      = "top-news-item__caption cell";
-        const spanClass         = "caption-wrapper";
-
         const noopLink          = "#/drive-test/";
 
         let items = Object.keys(this.state.items).map(idx => {
-            return  <a key={idx} href={noopLink} className={aClass}>
-                        <div className={imgWrapperClass}>
-                            <img className={imgClass} src={this.state.items[idx].img} width="460" height="260" alt="" ></img>
+            return  <a key={idx} href={noopLink} className={styles.link}>
+                        <div className={styles.cell}>
+                            <img className={styles.cellImage} src={this.state.items[idx].img} width="460" height="260" alt="" ></img>
                         </div>
-                        <div className={captionClass}><span className={spanClass}>{this.state.items[idx].caption}</span></div>
+                        <div className={styles.cell}><span className={styles.caption}>{this.state.items[idx].caption}</span></div>
                     </a>
         });
 
-        return <div className={newsListClass}>{items}</div>
+        return <div className={styles.list}>{items}</div>
     }
 }
