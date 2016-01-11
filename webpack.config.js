1c23f17d275bf8e8ca11794759edb66842c23a6b
designeng <denissavenok@gmail.com>
2016-01-11 16:00:30 +0300

    ExtractTextPlugin webpack loader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var LiveReloadPlugin = require('webpack-livereload-plugin');
+var LiveReloadPlugin    = require('webpack-livereload-plugin');
+var ExtractTextPlugin   = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: __dirname + '/src/index.js',
@@ -11,10 +12,16 @@ module.exports = {
                 test: /\.js/,
                 loader: 'babel',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/, 
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'),
+                exclude: /node_modules/
             }
         ]
     },
     plugins: [
+        new ExtractTextPlugin('public/css/style.css', { allChunks: true }),
         new LiveReloadPlugin({ port: 8082 })
     ],
     devtool: 'source-map'
