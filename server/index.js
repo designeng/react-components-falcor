024865422d2c35ca6482e813b1b3265e66bd408e
designeng <denissavenok@gmail.com>
2016-01-12 14:58:06 +0300

    mongoose connector added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 var app = require('./app');
+var db = require('./database');
+
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.set('port', process.env.PORT || 8080);
 var port = app.get('port');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("CONNECTED");
+});
 
 server.listen(port, err => {
     if (err) {
